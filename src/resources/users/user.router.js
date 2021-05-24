ecdf088fd03bcf9d3bb920010ee202e368a09bb7
ArseniyXaoc <xaoc190789@gmail.com>
2021-05-24 17:12:04 +0300

    feat: init Task 4

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
-// const { userValidate } = require('../../utils/validation');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
 });
 
@@ -14,17 +12,6 @@ router.route('/').post(async (req, res) => {
   const user =  await usersService.create(employers);
   res.status(201).json(User.toResponse(user));
 });
-
-// router.route('/').post(async (req, res) => {
-//   const { body } = req;
-//   const { error, status, data } = userValidate(body);
-//   if (error !== undefined) {
-//     res.status(status).json(data);
-//   } else {
-//     const user = await usersService.createUser(body);
-//     res.status(201).json(User.toResponse(body));
-//   // }
-// });
 
 router.route('/:id').get(async (req, res) => {
   const user = await usersService.getById(req.params.id);
