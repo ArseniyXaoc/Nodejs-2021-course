365f8ffef54579831946b10f7527db7009c05165
ArseniyXaoc <xaoc190789@gmail.com>
2021-05-25 11:01:13 +0300

    feat: add JSDoc to task.memory.repository.js

--- src/resources/task/tasks.memory.repository.js
+++ src/resources/task/tasks.memory.repository.js
@@ -1,22 +1,48 @@
 const { TASK } = require('../../common/in-memory');
 const Task = require('./tasks.model');
 
+/**
+ * Return all Tasks from TASKS db
+ * @returns {Promise<[]>} All Tasks
+ */
 const getAll = async () => TASK;
 
+/**
+ * Create new Task and add to TASK db
+ * @param boardId Board id for new Task
+ * @param body Task body from Task model should contain (id, title, order, description , userId, boardId, columnId)
+ * @returns {Promise<Task>} Created Task
+ */
 const create = async (boardId, body) => {
   const task = new Task({ ...body, boardId });
   await TASK.push(task)
   return task;
 };
 
+/**
+ *
+ * @param id Task Id
+ * @param data Data for updated Task field
+ * @returns {Promise<*>} Updated Task
+ */
 const update = async (id, data) => {
   const index = TASK.findIndex(item => item.id === id);
   TASK[index] = { ...TASK[index], ...data };
   return TASK[index];
 };
 
+/**
+ *
+ * @param id TAsk Id
+ * @returns {Promise<*>} Found Task by Id in TASK db
+ */
 const getById = async (id) => TASK.find(task => task.id === id);
 
+/**
+ *
+ * @param id Task id
+ * @returns {Promise<*[]|undefined>} Deleted task from TASK db of undefined if Task do not found
+ */
 const deleteId = async (id) => {
   const index = TASK.findIndex(item => item.id === id);
   if (index !== -1) {
