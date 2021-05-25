b0991ce8a81572658e83e590f983bbb3dac74e12
ArseniyXaoc <xaoc190789@gmail.com>
2021-05-25 11:24:43 +0300

    feat: add JSDoc to board.memory.repository.js

--- src/resources/boards/boards.memory.repository.js
+++ src/resources/boards/boards.memory.repository.js
@@ -2,23 +2,47 @@ const { BOARD } = require('../../common/in-memory');
 const { Board } = require('./boards.model');
 const { TASK } = require('../../common/in-memory');
 
+/**
+ * Get all Boars by id from BOARD db
+ * @returns {Promise<[]>} All Board from BOARD db
+ */
 const getAll = async () => BOARD;
 
+/**
+ * Create new Body in BOARD db
+ * @param body Body data {id, title, column: [{id, title, order}]}
+ * @returns {Promise<Board>}
+ */
 const create = async (body) => {
   const board = new Board(body);
   await BOARD.push(board)
   return board;
 };
 
+/**
+ * Update Board data by id in BOARD db
+ * @param id Board id
+ * @param data Board data {id, title, column: [{id, title, order}]}
+ * @returns {Promise<*>} found data in BOARD db
+ */
 const update = async (id, data) => {
   const index = BOARD.findIndex(item => item.id === id);
   BOARD[index] = { ...BOARD[index], ...data };
   return BOARD[index];
 };
 
+/**
+ * Get Board by id from BOARD db
+ * @param id Board id
+ * @returns {Promise<*>} found Board in BOARD db
+ */
 const getById = async (id) => BOARD.find(board => board.id === id);
 
-// eslint-disable-next-line consistent-return
+/**
+ * Delete all Tasks in deleted Board
+ * @param id Board id
+ * @returns {Promise<void>} Deleted Board by id BOARD db
+ */
 const deleteBoardTasks = async (id) => {
   while (TASK.findIndex((item) => item.boardId === id) !== -1) {
     TASK.findIndex((item, index) => {
@@ -28,6 +52,11 @@ const deleteBoardTasks = async (id) => {
   }
 }
 
+/**
+ * Delete Board by id
+ * @param id Board id
+ * @returns {Promise<*[]|undefined>} Deleted board from BOARD db or undefined
+ */
 const deleteId = async (id) => {
   deleteBoardTasks(id);
   const index = BOARD.findIndex(item => item.id === id);
