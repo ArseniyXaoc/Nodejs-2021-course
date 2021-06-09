19bad75201283c1dc26587ab620b895334f5fb7e
ArseniyXaoc <xaoc190789@gmail.com>
2021-06-09 15:28:15 +0300

    feat: add Dockerfile for server

--- src/resources/task/tasks.router.ts
+++ src/resources/task/tasks.router.ts
@@ -2,47 +2,62 @@ import { Router } from 'express';
 import taskService from './tasks.service';
 import { Task } from './tasks.model';
 /// <reference types="../../../custom" />
-const router = Router({mergeParams: true});
+const router = Router({ mergeParams: true });
 router.route('/').get(async (_req, res) => {
   const tasks = await taskService.getAll();
   res.json(tasks);
-})
-
-interface IboardId {
-  boardId: string
-}
-
-router.route('/').post(async (req, res) => {
-  const  {body} = req;
-  const boardIdS: IboardId = JSON.parse(JSON.stringify(req.params));
-  const { boardId } = boardIdS;
-  const task = await taskService.create(boardId, body);
-  res.status(201).json(Task.toResponse(task));
-})
-
-router.route('/:id').get(async (req, res) => {
-  const task = await taskService.getById(req.params.id);
-  if (task === undefined) {
-    res.status(404).json('task not found');
-  } else res.json(task);
 });
 
-router.route('/:id').put(async (req, res) => {
-  const task = await taskService.getById(req.params.id);
-  if (task === undefined) {
-    res.status(404).send('task not found');
-  } else {
-    await taskService.update(task.id, req.body);
-    res.json(task);
+interface IboardId {
+  boardId: string;
+}
+
+router.route('/').post(async (req, res, next) => {
+  try {
+    const { body } = req;
+    const boardIdS: IboardId = JSON.parse(JSON.stringify(req.params));
+    const { boardId } = boardIdS;
+    const task = await taskService.create(boardId, body);
+    res.status(201).json(Task.toResponse(task));
+  } catch (error) {
+    next(error);
   }
 });
 
-router.route('/:id').delete(async (req, res) => {
-  const task = await taskService.deleteId(req.params.id);
-  if (task === undefined) {
-    res.status(404).json('task not found');
-  } else res.status(204).json('The task has been deleted');
+router.route('/:id').get(async (req, res, next) => {
+  try {
+    const task = await taskService.getById(req.params.id);
+    if (task === undefined) {
+      res.status(404).json('task not found');
+    } else res.json(task);
+  } catch (error) {
+    next(error);
+  }
 });
 
+router.route('/:id').put(async (req, res, next) => {
+  try {
+    const task = await taskService.getById(req.params.id);
+    if (task === undefined) {
+      res.status(404).send('task not found');
+    } else {
+      await taskService.update(task.id, req.body);
+      res.json(task);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.route('/:id').delete(async (req, res, next) => {
+  try {
+    const task = await taskService.deleteId(req.params.id);
+    if (task === undefined) {
+      res.status(404).json('task not found');
+    } else res.status(204).json('The task has been deleted');
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
--- src/resources/users/user.router.ts
+++ src/resources/users/user.router.ts
@@ -1,43 +1,61 @@
 import { Router } from 'express';
-import {User} from './user.model';
+import { User } from './user.model';
 import usersService from './user.service';
 
 const router = Router();
-router.route('/').get(async (_req, res) => {
-  const users = await usersService.getAll();
-  res.json(users.map(User.toResponse));
-});
-
-router.route('/').post(async (req, res) => {
-  const employers = req.body;
-  const user =  await usersService.create(employers);
-  res.status(201).json(User.toResponse(user));
-});
-
-router.route('/:id').get(async (req, res) => {
-  const user = await usersService.getById(req.params.id);
-  if (user) {
-    res.json(User.toResponse(user));
-  } else
-   res.status(404).json('User not found');
-});
-
-router.route('/:id').put(async (req, res) => {
-  const user = await usersService.getById(req.params.id);
-  if (user) {
-    await usersService.update(user.id, req.body);
-    res.json(User.toResponse(user));
-  } else {
-    res.status(404).send('User not found');
+router.route('/').get(async (_req, res, next) => {
+  try {
+    const users = await usersService.getAll();
+    res.json(users.map(User.toResponse));
+  } catch (error) {
+    next(error);
   }
-  
 });
 
-router.route('/:id').delete(async (req, res) => {
-  const user = await usersService.deleteId(req.params.id);
-  if (user === undefined) {
-    res.status(404).json('User not found');
-  } else res.status(204).json('The user has been deleted');
+router.route('/').post(async (req, res, next) => {
+  try {
+    const employers = req.body;
+    const user = await usersService.create(employers);
+    res.status(201).json(User.toResponse(user));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.route('/:id').get(async (req, res, next) => {
+  try {
+    const user = await usersService.getById(req.params.id);
+    if (user) {
+      res.json(User.toResponse(user));
+    } else res.status(404).json('User not found');
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.route('/:id').put(async (req, res, next) => {
+  try {
+    const user = await usersService.getById(req.params.id);
+    if (user) {
+      await usersService.update(user.id, req.body);
+      res.json(User.toResponse(user));
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.route('/:id').delete(async (req, res, next) => {
+  try {
+    const user = await usersService.deleteId(req.params.id);
+    if (user === undefined) {
+      res.status(404).json('User not found');
+    } else res.status(204).json('The user has been deleted');
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
